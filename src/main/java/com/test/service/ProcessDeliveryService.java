ce5df645f48db3b7381f4d5b4ac430a9742f202a
lcarvajal <compucarvajal@gmail.com>
2020-06-28 17:08:50 -0500

    feat/processDelivery advance

---
+++ src/main/java/com/test/service/ProcessDeliveryService.java
@@ -0,0 +1,20 @@
+package com.test.service;
+
+import java.util.HashMap;
+
+public class ProcessDeliveryService {
+
+    private int xIni;
+    private int yIni;
+
+    public ProcessDeliveryService(){
+
+    }
+
+    public void processOrders(HashMap orderByDrone){
+
+    }
+
+
+
+}
